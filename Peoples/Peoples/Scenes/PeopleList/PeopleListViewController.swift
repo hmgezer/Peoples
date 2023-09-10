300c9178d49f670a5ed42fafbb712ddc7afc4f5c
Hüseyin Murat Gezer <murat.gezer@mobven.com>
2023-09-10 16:31:57 +0300

    people page mvvm files added

---
+++ Peoples/Peoples/Scenes/PeopleList/PeopleListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PeopleListViewController.swift
+//  Peoples
+//
+//  Created by Hüseyin Murat Gezer on 10.09.2023.
+//
+
+import UIKit
+
+class PeopleListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Peoples/Peoples/Scenes/PeopleList/PeopleListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PeopleListViewModel.swift
+//  Peoples
+//
+//  Created by Hüseyin Murat Gezer on 10.09.2023.
+//
+
+import Foundation
