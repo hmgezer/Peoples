772050e5dc12f0a374bc061894ffba2752794279
Hüseyin Murat Gezer <murat.gezer@mobven.com>
2023-09-10 18:09:01 +0300

    people table view cell added.

---
+++ Peoples/Peoples/UIComponents/PeopleCardTableViewCell/PeopleCardTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PeopleCardTableViewCell.swift
+//  Peoples
+//
+//  Created by Hüseyin Murat Gezer on 10.09.2023.
+//
+
+import UIKit
+
+class PeopleCardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
